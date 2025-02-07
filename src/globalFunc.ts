12f5d71560e072fc56f79f547ae15af11d3b7521
Andrew Fenn <afenn666@gmail.com>
2025-02-07 21:35:51 +0000

    refactor: tidy up the fetchUser code

--- src/globalFunc.ts
+++ src/globalFunc.ts
@@ -15,7 +15,7 @@ globalThis.stringToPairsPath = function(param: string): PathsWithMethod<paths, "
 globalThis.refreshRetryAPIroute = async function (route: PathsWithMethod<paths, "get">): Promise<boolean> {
   let errorOcurred = false;
   try {
-    const { error, response } = await client.POST("/api/refresh", {});
+    const { response } = await client.POST("/api/refresh", {});
     if (response.status == 401) {
       // The refresh token was invalid, could not refresh so back to login. 
       // This has to be done for every fetch
@@ -23,7 +23,7 @@ globalThis.refreshRetryAPIroute = async function (route: PathsWithMethod<paths, 
       window.location.href = "/login";
     } else if (response.status == 201) {
       // retry the specified route
-      const {data, error, response } = await client.GET(route, {},);
+      const { data, response } = await client.GET(route, {},);
       console.log(data);
       if (response.status == 401) {
       // MSAL client may no longer have user in cache, no other option other than to log out
