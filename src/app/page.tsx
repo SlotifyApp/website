a374ec9a79585f3b798b9135f089bcdaca4d7730
Andrew Fenn <afenn666@gmail.com>
2025-02-07 15:27:46 +0000

    refactor: rewrite page.tsx to better match documentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,20 +6,20 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   useEffect(() => {
     const fetchUser = async () => {
-      // Detects whether the access token or refresh token are valid, if either one is then
-      // redirect to /dashboard. Done through:
-      // Attempt to get /user route, if 200 then redirect to /dashboard
-      // if 401 then attempt to refresh
-      // If refresh fails then do nothing, if it succeeds then redirect to
-      // /dashboard
+      // Detects whether the access token or refresh token are valid.
+      // If either one is, then redirect to /dashboard.
+      // Done through:
+      // 1. Attempt to get /user route, if 200 then redirect to /dashboard
+      // 2. if 401 then attempt to refresh
+      // 3. If refresh fails then do nothing, if it succeeds then redirect to /dashboard
       const { response } = await client.GET("/api/users/me", {});
-      if (response.status == 401) {
+      if (response.status == 200) {
+        window.location.href = "/dashboard";
+      } else if (response.status == 401) {
         const { response } = await client.POST("/api/refresh", {});
         if (response.status == 201) {
           window.location.href = "/dashboard";
         }
-      } else if (response.status == 200) {
-        window.location.href = "/dashboard";
       }
     };
     fetchUser();
