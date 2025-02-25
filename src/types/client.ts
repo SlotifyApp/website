cf76af819d34ab76a333bbf6bc2255f0115b2187
Saath Satheeshkumar <saathsatheesh@gmail.com>
2025-02-25 11:57:42 +0000

    Update shared and generate new swagger types

--- src/types/client.ts
+++ src/types/client.ts
@@ -1,31 +1,70 @@
 import { makeApi, Zodios, type ZodiosOptions } from "@zodios/core";
 import { z } from "zod";
 
-type CalendarEvent = {
-  id?: string | undefined;
-  attendees: Array<Attendee>;
-  body?: string | undefined;
-  created?: string | undefined;
-  endTime?: (string | null) | undefined;
-  isCancelled?: boolean | undefined;
-  joinURL?: (string | null) | undefined;
-  locations: Array<Location>;
-  organizer?: string | undefined;
-  startTime?: (string | null) | undefined;
-  subject?: string | undefined;
-  webLink?: string | undefined;
+type TimeConstraint = {
+  activityDomain?: string | undefined;
+  timeSlots: Array<MeetingTimeSlot>;
 };
-type Attendee = Partial<{
-  email: string | null;
-  type: "required" | "optional" | "resource" | null;
-  responseStatus:
-    | "none"
-    | "organizer"
-    | "entativelyAccepted"
-    | "accepted"
-    | "declined"
-    | "notResponded"
-    | null;
+type MeetingTimeSlot = {
+  end: string;
+  start: string;
+};
+type SchedulingSlotsBodySchema = {
+  attendees: Array<AttendeeBase>;
+  meetingName: string;
+  meetingDuration: string;
+  isOrganizerOptional: boolean;
+  locationConstraint: LocationConstraint;
+  minimumAttendeePercentage?: number | undefined;
+  maxCandidates?: number | undefined;
+  timeConstraint: TimeConstraint;
+};
+type AttendeeBase = {
+  emailAddress: EmailAddress;
+  attendeeType: AttendeeType;
+};
+type EmailAddress = {
+  address: string;
+  name: string;
+};
+type AttendeeType = "required" | "optional" | "resource";
+type LocationConstraint = Partial<{
+  isRequired: boolean;
+  locations: Array<LocationConstraintItem>;
+  suggestLocation: boolean;
+}>;
+type LocationConstraintItem = {
+  displayName: string;
+  resolveAvailability: boolean;
+  locationEmailAddress?: string | undefined;
+  address: PhysicalAddress;
+};
+type PhysicalAddress = Partial<{
+  city: string;
+  countryOrRegion: string;
+  postalCode: string;
+  state: string;
+  street: string;
+}>;
+type AttendeeAvailability = {
+  availability: FreeBusyStatus;
+  attendee: AttendeeBase;
+};
+type FreeBusyStatus =
+  | "free"
+  | "tentative"
+  | "busy"
+  | "oof"
+  | "workingElsewhere"
+  | "unknown";
+type MeetingTimeSuggestion = Partial<{
+  attendeeAvailability: Array<AttendeeAvailability>;
+  confidence: number;
+  locations: Array<Location>;
+  meetingTimeSlot: MeetingTimeSlot;
+  order: number;
+  organizerAvailability: string;
+  suggestionReason: string;
 }>;
 type Location = Partial<{
   id: string;
@@ -44,6 +83,42 @@ type Location = Partial<{
     | null;
   street: string | null;
 }>;
+type SchedulingSlotsSuccessResponseBody = Partial<{
+  emptySuggestionsReason: EmptySuggestionsReason;
+  meetingTimeSuggestions: Array<MeetingTimeSuggestion>;
+}>;
+type EmptySuggestionsReason =
+  | "attendeesUnavailable"
+  | "attendeesUnavailableOrUnknown"
+  | "locationsUnavailable"
+  | "organizerUnavailable"
+  | "unknown";
+type Attendee = Partial<{
+  email: string | null;
+  attendeeType: AttendeeType;
+  responseStatus:
+    | "none"
+    | "organizer"
+    | "entativelyAccepted"
+    | "accepted"
+    | "declined"
+    | "notResponded"
+    | null;
+}>;
+type CalendarEvent = {
+  id?: string | undefined;
+  attendees: Array<Attendee>;
+  body?: string | undefined;
+  created?: string | undefined;
+  endTime?: (string | null) | undefined;
+  isCancelled?: boolean | undefined;
+  joinURL?: (string | null) | undefined;
+  locations: Array<Location>;
+  organizer?: string | undefined;
+  startTime?: (string | null) | undefined;
+  subject?: string | undefined;
+  webLink?: string | undefined;
+};
 
 const Notification = z
   .object({
@@ -52,10 +127,11 @@ const Notification = z
     created: z.string().datetime({ offset: true }),
   })
   .passthrough();
+const AttendeeType = z.enum(["required", "optional", "resource"]);
 const Attendee: z.ZodType<Attendee> = z
   .object({
     email: z.string().email().nullable(),
-    type: z.enum(["required", "optional", "resource"]).nullable(),
+    attendeeType: AttendeeType,
     responseStatus: z
       .enum([
         "none",
@@ -124,9 +200,104 @@ const UserCreate = z
   .passthrough();
 const Team = z.object({ id: z.number().int(), name: z.string() }).passthrough();
 const TeamCreate = z.object({ name: z.string() }).passthrough();
+const EmailAddress: z.ZodType<EmailAddress> = z
+  .object({ address: z.string().email(), name: z.string() })
+  .passthrough();
+const AttendeeBase: z.ZodType<AttendeeBase> = z
+  .object({ emailAddress: EmailAddress, attendeeType: AttendeeType })
+  .passthrough();
+const PhysicalAddress = z
+  .object({
+    city: z.string(),
+    countryOrRegion: z.string(),
+    postalCode: z.string(),
+    state: z.string(),
+    street: z.string(),
+  })
+  .partial()
+  .passthrough();
+const LocationConstraintItem: z.ZodType<LocationConstraintItem> = z
+  .object({
+    displayName: z.string(),
+    resolveAvailability: z.boolean(),
+    locationEmailAddress: z.string().optional(),
+    address: PhysicalAddress,
+  })
+  .passthrough();
+const LocationConstraint: z.ZodType<LocationConstraint> = z
+  .object({
+    isRequired: z.boolean(),
+    locations: z.array(LocationConstraintItem),
+    suggestLocation: z.boolean(),
+  })
+  .partial()
+  .passthrough();
+const MeetingTimeSlot: z.ZodType<MeetingTimeSlot> = z
+  .object({
+    end: z.string().datetime({ offset: true }),
+    start: z.string().datetime({ offset: true }),
+  })
+  .passthrough();
+const TimeConstraint: z.ZodType<TimeConstraint> = z
+  .object({
+    activityDomain: z.string().optional(),
+    timeSlots: z.array(MeetingTimeSlot),
+  })
+  .passthrough();
+const SchedulingSlotsBodySchema: z.ZodType<SchedulingSlotsBodySchema> = z
+  .object({
+    attendees: z.array(AttendeeBase),
+    meetingName: z.string(),
+    meetingDuration: z.string(),
+    isOrganizerOptional: z.boolean(),
+    locationConstraint: LocationConstraint,
+    minimumAttendeePercentage: z.number().optional(),
+    maxCandidates: z.number().int().optional(),
+    timeConstraint: TimeConstraint,
+  })
+  .passthrough();
+const EmptySuggestionsReason = z.enum([
+  "attendeesUnavailable",
+  "attendeesUnavailableOrUnknown",
+  "locationsUnavailable",
+  "organizerUnavailable",
+  "unknown",
+]);
+const FreeBusyStatus = z.enum([
+  "free",
+  "tentative",
+  "busy",
+  "oof",
+  "workingElsewhere",
+  "unknown",
+]);
+const AttendeeAvailability: z.ZodType<AttendeeAvailability> = z
+  .object({ availability: FreeBusyStatus, attendee: AttendeeBase })
+  .passthrough();
+const MeetingTimeSuggestion: z.ZodType<MeetingTimeSuggestion> = z
+  .object({
+    attendeeAvailability: z.array(AttendeeAvailability),
+    confidence: z.number(),
+    locations: z.array(Location),
+    meetingTimeSlot: MeetingTimeSlot,
+    order: z.number().int(),
+    organizerAvailability: z.string(),
+    suggestionReason: z.string(),
+  })
+  .partial()
+  .passthrough();
+const SchedulingSlotsSuccessResponseBody: z.ZodType<SchedulingSlotsSuccessResponseBody> =
+  z
+    .object({
+      emptySuggestionsReason: EmptySuggestionsReason,
+      meetingTimeSuggestions: z.array(MeetingTimeSuggestion),
+    })
+    .partial()
+    .passthrough();
 
 export const schemas = {
   Notification,
+  AttendeeType,
   Attendee,
   Location,
   CalendarEvent,
@@ -134,6 +305,19 @@ export const schemas = {
   UserCreate,
   Team,
   TeamCreate,
+  EmailAddress,
+  AttendeeBase,
+  PhysicalAddress,
+  LocationConstraintItem,
+  LocationConstraint,
+  MeetingTimeSlot,
+  TimeConstraint,
+  SchedulingSlotsBodySchema,
+  EmptySuggestionsReason,
+  FreeBusyStatus,
+  AttendeeAvailability,
+  MeetingTimeSuggestion,
+  SchedulingSlotsSuccessResponseBody,
 };
 
 const endpoints = makeApi([
@@ -318,6 +502,37 @@ const endpoints = makeApi([
         status: 401,
         description: `Access token is missing or invalid`,
         schema: z.void(),
+      },
+    ],
+  },
+  {
+    method: "post",
+    path: "/api/scheduling/slots",
+    alias: "PostAPISchedulingFree",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "body",
+        type: "Body",
+        schema: SchedulingSlotsBodySchema,
+      },
+    ],
+    response: SchedulingSlotsSuccessResponseBody,
+    errors: [
+      {
+        status: 400,
+        description: `Bad request (e.g., invalid event data)`,
+        schema: z.string(),
+      },
+      {
+        status: 401,
+        description: `Access token is missing or invalid`,
+        schema: z.void(),
+      },
+      {
+        status: 500,
+        description: `Something went wrong internally`,
+        schema: z.string(),
       },
     ],
   },
